73ad89a9d05bceecf2edeaa37b4348aa8f294a57
noah <noah@ovyl.io>
2022-12-01 17:31:51 -0600

    Add help text to cmd struct

--- src/cli_defs.h
+++ src/cli_defs.h
@@ -27,25 +27,26 @@ typedef void (*println_func_ptr_t)(const char * format, ...);
 
 /*!
  * @brief Command structure, consisting of a name and function pointer.
- */ 
+ */
 typedef struct
 {
     char *cmd;           /* Command name.                            */
     cmd_func_ptr_t func; /* Function pointer to associated function. */
+    char *help_text;    /* Help text.                                */
 } cmd_t;
 
 /*!
  * @brief Command-line interface handle structure.
  */
 typedef struct
-{    
+{
     println_func_ptr_t println; /* Function pointer to user defined println function.      */
     cmd_t *cmd_tbl;             /* Pointer to series of commands which are to be accepted. */
     size_t cmd_cnt;             /* Number of commands in cmd_tbl.                          */
 } cli_t;
 
 /*!
- * @brief Struct for holding received data 
+ * @brief Struct for holding received data
  */
 typedef struct
 {
